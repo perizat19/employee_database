fc893864214f838fabf0c854a54839538a3faa1c
perizat19 <150646975+perizat19@users.noreply.github.com>
2025-04-08 15:14:08 +0600

    main.py

--- main.py
+++ main.py
@@ -5,17 +5,17 @@ from employee import Employee
 dao = EmployeeDAO('employeedb.sqlite')
 
 e = dao.get_by_id(5)
-print(e)
+print(f'Get by id: \n{e}')
 
 e = dao.insert(Employee(230121004, 'Peri', 'Data analyst', '400000', '21-04-2025'))
-print(e)
+print(f'Insert:\n {e}')
 
 e = dao.get_all()
-print(e)
+print(f'Get all:\n {e}')
 
-employee = Employee(240221003, 'John', 'Python developer', '300000', '23-09-2020')
+employee = Employee(5, 'John', 'Python developer', '300000', '23-09-2020')
 e = dao.update(employee)
-print(e)
+print(f'Updated: {e}')
 
-e = dao.delete(240221003, 'John', 'Python developer', '300000', '23-09-2020')
+e = dao.delete(2)
 print(e)
